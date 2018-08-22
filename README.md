265e92c3ef367fcdad23de60ac2384b358d481f4
Noman <noman@noman.land>
2018-08-21 20:48:04 -0400

    Make gulp work after many updates

---
+++ README.md
@@ -0,0 +1,10 @@
+# Status.im website
+
+https://status.im
+
+### Development mode
+
+1. Install the latest nodejs, either via (the installer)[https://nodejs.org/en/] or (nvm)[https://github.com/creationix/nvm]
+2. Install gulp-cli with `npm i -g gulp-cli`
+3. Run the site locally with `gulp`
+4. Site will open at `localhost:3000`
--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,65 @@
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync').create();
-var sass         = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp         = require('gulp');
+const browserSync  = require('browser-sync').create();
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-var browserify   = require('browserify');
-var source       = require('vinyl-source-stream');
-var streamify    = require('gulp-streamify');
-var babel        = require('gulp-babel');
-var tap          = require('gulp-tap');
-var buffer       = require('gulp-buffer');
-var sourcemaps   = require('gulp-sourcemaps');
+const browserify   = require('browserify');
+const source       = require('vinyl-source-stream');
+const streamify    = require('gulp-streamify');
+const babel        = require('gulp-babel');
+const tap          = require('gulp-tap');
+const buffer       = require('gulp-buffer');
+const sourcemaps   = require('gulp-sourcemaps');
 
-var del          = require('del');
-var gutil        = require('gulp-util');
-var uglify       = require('gulp-uglify');
+const del          = require('del');
+const gutil        = require('gulp-util');
+const uglify       = require('gulp-uglify');
 
 var imagemin     = require('gulp-imagemin');
 
-gulp.task('serve', ['sass', 'imagemin', 'js'], function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("src/scss/*.scss", ['sass']);
-    gulp.watch("src/js/*.js", ['js']);
-    gulp.watch("src/img/**/*", ['imagemin']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
-
 gulp.task('js', function () {
   return gulp.src('src/**/*.js', {read: false})
-             .pipe(tap(function (file) {
-                gutil.log('bundling ' + file.path)
-                file.contents = browserify(file.path, {debug: true}).bundle()
-              }))
-              .pipe(streamify(babel({ presets: ['es2015'] })))
-              .pipe(buffer())
-              .pipe(sourcemaps.init({loadMaps: true}))
-              .pipe(uglify())
-              .pipe(sourcemaps.write('./'))
-              .pipe(gulp.dest('dist'))
-              .pipe(browserSync.stream());
+    .pipe(tap(function (file) {
+      gutil.log('bundling ' + file.path);
+      file.contents = browserify(file.path, { debug: true }).bundle();
+    }))
+    .pipe(streamify(babel({ presets: ['es2015'] })))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('sass', function() {
-    return gulp.src("src/scss/main.scss")
-               .pipe(sass())
-               .on('error', gutil.log)
-               .pipe(autoprefixer({ browsers: ['last 3 versions'], cascade: false }))
-               .pipe(gulp.dest("dist/css"))
-               .pipe(browserSync.stream());
+  return gulp.src("src/scss/main.scss")
+    .pipe(sass())
+    .on('error', gutil.log)
+    .pipe(autoprefixer({ browsers: ['last 3 versions'], cascade: false }))
+    .pipe(gulp.dest("dist/css"))
+.pipe(browserSync.stream());
 });
 
 gulp.task('imagemin', function() {
-  gulp.src('src/img/**/*')
-      .pipe(imagemin())
-      .pipe(gulp.dest('dist/img'))
+  return gulp.src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
 });
 
 gulp.task('cleanUp', function() {
-  del.sync('dist/*')
+  return del('dist/*');
 });
 
-gulp.task('default', ['cleanUp', 'serve']);
+gulp.task('serve', gulp.parallel('sass', 'imagemin', 'js', function() {
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("src/scss/*.scss", gulp.series('sass'));
+  gulp.watch("src/js/*.js", gulp.series('js'));
+  gulp.watch("src/img/**/*", gulp.series('imagemin'));
+  gulp.watch("./*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('cleanUp', 'serve'));
