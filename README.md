5e67adac062ce16b4745acfffeb33aa32b479fad
noman <27938023+noman-land@users.noreply.github.com>
2018-09-16 22:45:22 -0400

    Fixing most npm vulnerabilities and make README (#70)
    * Update deps to fix many vulnerabilities
    
    * Make gulp work after many updates
    
    * Return to vars and fix indentation
    
    * Be clearer about node version
    
    * Fix links in README
    
    * Make sure gulp is installed
    
    * More info in README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Status.im website
+
+https://status.im
+
+### Development mode
+
+1. Install nodejs 9, either via [the installer](https://nodejs.org/en/) or [nvm](https://github.com/creationix/nvm)
+2. Install gulp and gulp-cli globally with `npm i -g gulp gulp-cli`
+3. Run the site locally with `gulp` in the root of the project folder
+4. Site will open at `localhost:3000`
--- gulpfile.js
+++ gulpfile.js
@@ -17,50 +17,49 @@ var uglify       = require('gulp-uglify');
 
 var imagemin     = require('gulp-imagemin');
 
-gulp.task('serve', ['sass', 'imagemin', 'js'], function() {
-
-    browserSync.init({
-        server: "./"
-    })
-
-    gulp.watch("src/scss/*.scss", ['sass']);
-    gulp.watch("src/js/*.js", ['js']);
-    gulp.watch("src/img/**/*", ['imagemin']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
-})
-
 gulp.task('js', function () {
   return gulp.src('src/**/*.js', {read: false})
-             .pipe(tap(function (file) {
-                gutil.log('bundling ' + file.path)
-                file.contents = browserify(file.path, {debug: true}).bundle()
-              }))
-              .pipe(streamify(babel({ presets: ['es2015'] })))
-              .pipe(buffer())
-              .pipe(sourcemaps.init({loadMaps: true}))
-              .pipe(uglify())
-              .pipe(sourcemaps.write('./'))
-              .pipe(gulp.dest('dist'))
-              .pipe(browserSync.stream());
-})
+    .pipe(tap(function (file) {
+      gutil.log('bundling ' + file.path);
+      file.contents = browserify(file.path, { debug: true }).bundle();
+    }))
+    .pipe(streamify(babel({ presets: ['es2015'] })))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+});
 
 gulp.task('sass', function() {
-    return gulp.src("src/scss/main.scss")
-               .pipe(sass())
-               .on('error', gutil.log)
-               .pipe(autoprefixer({ browsers: ['last 3 versions'], cascade: false }))
-               .pipe(gulp.dest("dist/css"))
-               .pipe(browserSync.stream());
-})
+  return gulp.src("src/scss/main.scss")
+    .pipe(sass())
+    .on('error', gutil.log)
+    .pipe(autoprefixer({ browsers: ['last 3 versions'], cascade: false }))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+});
 
 gulp.task('imagemin', function() {
-  gulp.src('src/img/**/*')
-      .pipe(imagemin())
-      .pipe(gulp.dest('dist/img'))
-})
+  return gulp.src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+});
 
 gulp.task('cleanUp', function() {
-  del.sync('dist/*')
-})
+  return del('dist/*');
+});
 
-gulp.task('default', ['cleanUp', 'serve']);
+gulp.task('serve', gulp.parallel('sass', 'imagemin', 'js', function() {
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("src/scss/*.scss", gulp.series('sass'));
+  gulp.watch("src/js/*.js", gulp.series('js'));
+  gulp.watch("src/img/**/*", gulp.series('imagemin'));
+  gulp.watch("./*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('cleanUp', 'serve'));
